0c794eaca45207a875bbbebe65a9db8e8f013acb
flash159483 <seungwonjung0313@gmail.com>
2023-09-03 15:27:21 +0900

    Add navigation using button (#1)

--- app/src/main/java/com/lighthouse/multi_module_navigation/MainActivity.kt
+++ app/src/main/java/com/lighthouse/multi_module_navigation/MainActivity.kt
@@ -2,9 +2,11 @@ package com.lighthouse.multi_module_navigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import com.lighthouse.home.ResultFragment
 import com.lighthouse.multi_module_navigation.databinding.ActivityMainBinding
 import com.lighthouse.navigation.NavigationFlow
 import com.lighthouse.navigation.Navigator
@@ -25,11 +27,27 @@ class MainActivity @Inject constructor() : AppCompatActivity(), ToFlowNavigatabl
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
 
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when(destination.id) {
+                com.lighthouse.home.R.id.resultFragment -> hideBottomNav()
+                com.lighthouse.home.R.id.homeFragment -> showBottomNav()
+                com.lighthouse.setting.R.id.settingFragment -> showBottomNav()
+            }
+        }
+
         navigator.navController = navController
         binding.bottomNav.setupWithNavController(navController)
     }
 
     override fun navigateToFlow(flow: NavigationFlow) {
         navigator.navigateToFlow(flow)
+    }
+
+    private fun hideBottomNav() {
+        binding.bottomNav.visibility = View.GONE
+    }
+
+    private fun showBottomNav() {
+        binding.bottomNav.visibility = View.VISIBLE
     }
 }
