079f8c84fd34ab3161212818f6b4f13cfba5dc68
flash159483 <seungwonjung0313@gmail.com>
2023-09-03 13:50:58 +0900

    Implement UI (#1)

--- app/src/main/java/com/lighthouse/multi_module_navigation/MainActivity.kt
+++ app/src/main/java/com/lighthouse/multi_module_navigation/MainActivity.kt
@@ -2,10 +2,34 @@ package com.lighthouse.multi_module_navigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.lighthouse.multi_module_navigation.databinding.ActivityMainBinding
+import com.lighthouse.navigation.NavigationFlow
+import com.lighthouse.navigation.Navigator
+import com.lighthouse.navigation.ToFlowNavigatable
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
-class MainActivity : AppCompatActivity() {
+@AndroidEntryPoint
+class MainActivity @Inject constructor() : AppCompatActivity(), ToFlowNavigatable {
+    private lateinit var binding: ActivityMainBinding
+
+    @Inject
+    lateinit var navigator: Navigator
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        navigator.navController = navController
+        binding.bottomNav.setupWithNavController(navController)
+    }
+
+    override fun navigateToFlow(flow: NavigationFlow) {
+        navigator.navigateToFlow(flow)
     }
 }
