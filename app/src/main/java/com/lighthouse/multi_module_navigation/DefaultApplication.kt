dd2f1f62704bee49295013cb3b8f90903ada251b
flash159483 <seungwonjung0313@gmail.com>
2023-09-03 13:53:17 +0900

    Add di for navigation (#1)

---
+++ app/src/main/java/com/lighthouse/multi_module_navigation/DefaultApplication.kt
@@ -0,0 +1,8 @@
+package com.lighthouse.multi_module_navigation
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class DefaultApplication : Application() {
+}
---
+++ app/src/main/java/com/lighthouse/multi_module_navigation/NavigationModule.kt
@@ -0,0 +1,18 @@
+package com.lighthouse.multi_module_navigation
+
+import com.lighthouse.navigation.Navigator
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NavigationModule {
+    @Provides
+    @Singleton
+    fun provideNavigation(): Navigator {
+        return Navigator()
+    }
+}
